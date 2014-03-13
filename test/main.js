7daaecfb291c6fe022a6af44db165fb653e09e8e
fr0m <fr0m@qq.com>
2014-03-13 13:46:53 +0800

    Update

--- test/main.js
+++ test/main.js
@@ -1,6 +1,6 @@
 $(function(){
 	var div = ""
-	for(var i = 100; i--;){
+	for(var i = 200; i--;){
 		div = div + '<div style="float : left; margin : 10px; width : 180px; height : 170px; background-color : #cd' + Math.floor(Math.random() * 10) + ';"></div>';
 	}
 	$(".wrap").prepend(div);
